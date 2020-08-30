ba526622e392587ef11cfba9a4d82bcb4ad317c5
dherendra dev <dheerudev101@gmail.com>
2020-08-30 09:19:35 +0530

    Airbnb Clone React and MUI

---
+++ build/precache-manifest.59a61701b38a5512840422a1f0a70f66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bcd40a8c0e8fce1af7a50f9a6faaae2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04b3080ac563feb6bf3a",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "d6f1e1e96d660a607057",
+    "url": "/static/css/main.1ac31e32.chunk.css"
+  },
+  {
+    "revision": "04b3080ac563feb6bf3a",
+    "url": "/static/js/2.be458b3f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.be458b3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f1e1e96d660a607057",
+    "url": "/static/js/main.835fb5f4.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
